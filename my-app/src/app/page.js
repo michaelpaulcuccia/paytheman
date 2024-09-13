dd498a2d1edce623248a97617ce8772638f9880d
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-09-13 13:39:59 -0500

    next with styledcomponents

---
+++ my-app/src/app/page.js
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+import styled from "styled-components";
+
+const Root = styled.div`
+  background: black;
+  color: white;
+`;
+
+export default function page() {
+  return <Root>page</Root>;
+}
