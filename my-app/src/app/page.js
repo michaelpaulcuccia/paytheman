e0e785af673ba11453985f4daae56d517ce14a53
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-09-13 16:28:36 -0500

    adds scrollbar on hover at min-width

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -4,9 +4,31 @@ import styled from "styled-components";
 import Link from "next/link";
 import { data } from "../../data";
 
+const TableContainer = styled.div`
+  overflow-x: auto;
+  max-width: 100%;
+
+  &::-webkit-scrollbar {
+    height: 0.5rem;
+    width: 0.25rem;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 0.5rem;
+    background-color: #0004;
+    visibility: hidden;
+  }
+
+  &:hover::-webkit-scrollbar-thumb {
+    visibility: visible;
+  }
+`;
+
 const Table = styled.table`
   width: 100%;
   border-collapse: collapse;
+  table-layout: auto;
+  min-width: 865px; /* Minimum width for horizontal scrolling */
 `;
 
 const Th = styled.th`
@@ -20,6 +42,7 @@ const Td = styled.td`
   border: 1px solid #ddd;
   padding: 8px;
   text-align: left;
+  white-space: nowrap;
 `;
 
 const Tr = styled.tr`
@@ -38,48 +61,53 @@ const DebtsListItem = styled.li`
 
 const Index = () => {
   return (
-    <Table>
-      <thead>
-        <tr>
-          <Th>Name</Th>
-          <Th>Primary Email</Th>
-          <Th>Primary Cell</Th>
-          <Th>Debts</Th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((person, index) => (
-          <Tr key={person.id} $isEven={index % 2 === 0}>
-            <Td>
-              {person.name.firstName}{" "}
-              {person.name.middleName ? person.name.middleName + " " : ""}
-              {person.name.lastName}
-            </Td>
-            <Td>{person.communication.email.primary}</Td>
-            <Td>{person.communication.phone.cell}</Td>
-            <Td>
-              <DebtsList>
-                {person.debts && person.debts.length > 0 ? (
-                  person.debts.map((debt) => (
-                    <DebtsListItem key={debt.debtId}>
-                      <Link href={`/debtinfo/${debt.debtId}`}>
-                        <p
-                          style={{ color: "blue", textDecoration: "underline" }}
-                        >
-                          {debt.debtor.company}
-                        </p>
-                      </Link>
-                    </DebtsListItem>
-                  ))
-                ) : (
-                  <DebtsListItem>No debts</DebtsListItem>
-                )}
-              </DebtsList>
-            </Td>
-          </Tr>
-        ))}
-      </tbody>
-    </Table>
+    <TableContainer>
+      <Table>
+        <thead>
+          <tr>
+            <Th>Name</Th>
+            <Th>Primary Email</Th>
+            <Th>Primary Cell</Th>
+            <Th>Debts</Th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((person, index) => (
+            <Tr key={person.id} $isEven={index % 2 === 0}>
+              <Td>
+                {person.name.firstName}{" "}
+                {person.name.middleName ? person.name.middleName + " " : ""}
+                {person.name.lastName}
+              </Td>
+              <Td>{person.communication.email.primary}</Td>
+              <Td>{person.communication.phone.cell}</Td>
+              <Td>
+                <DebtsList>
+                  {person.debts && person.debts.length > 0 ? (
+                    person.debts.map((debt) => (
+                      <DebtsListItem key={debt.debtId}>
+                        <Link href={`/debtinfo/${debt.debtId}`}>
+                          <p
+                            style={{
+                              color: "blue",
+                              textDecoration: "underline",
+                            }}
+                          >
+                            {debt.debtor.company}
+                          </p>
+                        </Link>
+                      </DebtsListItem>
+                    ))
+                  ) : (
+                    <DebtsListItem>No debts</DebtsListItem>
+                  )}
+                </DebtsList>
+              </Td>
+            </Tr>
+          ))}
+        </tbody>
+      </Table>
+    </TableContainer>
   );
 };
 
