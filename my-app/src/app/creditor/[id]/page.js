56353d2e539aaec3f33d9675bb96228d2781a7a2
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-09-19 16:14:28 -0500

    see TOO on creditor/id/page

--- my-app/src/app/creditor/[id]/page.js
+++ my-app/src/app/creditor/[id]/page.js
@@ -1,10 +1,36 @@
 "use client";
 import React from "react";
 import { usePathname } from "next/navigation";
+import { data } from "../../../../data";
+import { TwoCols, Tab } from "../../../../components/Atoms";
+import { IoIosBusiness } from "react-icons/io";
 
 export default function page() {
   const pathname = usePathname();
   const itemID = pathname.split("/creditor/")[1];
   const decodedName = decodeURIComponent(itemID);
-  return <div>Welcome to info about {decodedName}</div>;
+
+  const findDebtorByCompany = (data, companyName) => {
+    return data.find((item) =>
+      item.debts.some((debt) => debt.debtor.company === companyName)
+    );
+  };
+
+  const debtor = findDebtorByCompany(data, decodedName);
+  console.log(debtor);
+  //TO DO: pass the debtID NOT the creditor name to page for easier search
+
+  return (
+    <div>
+      <TwoCols>
+        <Tab>
+          <div className="icon-container">
+            <IoIosBusiness />
+          </div>
+          <span>Company Name:</span>
+          {decodedName}
+        </Tab>
+      </TwoCols>
+    </div>
+  );
 }
